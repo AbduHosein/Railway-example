d6af43f4dfc653db00814c94454c50664b3bc07d
Abdu <hoseina@bc.edu>
2023-05-11 11:08:12 -0400

    Whitenoise middleware

--- ExampleProject/ExampleProject/settings.py
+++ ExampleProject/ExampleProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os.path
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,13 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
+
+
 
--- ExampleProject/ExampleProject/urls.py
+++ ExampleProject/ExampleProject/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.contrib.staticfiles.views import serve as serve_static
+
+def _static_butler(request, path, **kwargs):
+    """
+    Serve static files using the django static files configuration
+    WITHOUT collectstatic. This is slower, but very useful for API 
+    only servers where the static files are really just for /admin
+
+    Passing insecure=True allows serve_static to process, and ignores
+    the DEBUG=False setting
+    """
+    return serve_static(request, path, insecure=True, **kwargs)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #re_path(r'static/(.+)', _static_butler)
 ]
